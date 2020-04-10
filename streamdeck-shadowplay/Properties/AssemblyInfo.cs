32f73b17e71d1b01a4a09f52b8e409f999c6f8e0
BarRaider <46548278+BarRaider@users.noreply.github.com>
2020-04-09 22:26:04 -0700

    Update AssemblyInfo.cs

--- streamdeck-shadowplay/Properties/AssemblyInfo.cs
+++ streamdeck-shadowplay/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("streamdeck-shadowplay")]
+[assembly: AssemblyTitle("com.barraider.shadowplay")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("streamdeck-shadowplay")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("com.barraider.shadowplay")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
